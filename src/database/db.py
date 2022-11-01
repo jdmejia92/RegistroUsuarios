b1b5645740f5f160637cbcf5e6c49041ec14f262
Juan Daniel Mejia Hoyos <jdmejia92@icloud.com>
2022-10-31 21:44:58 -0400

    ssh tunnel

--- src/database/db.py
+++ src/database/db.py
@@ -1,10 +1,21 @@
 import psycopg2
 from psycopg2 import DatabaseError
 from decouple import config
+import sshtunnel
 
 
 def get_connection():
     try:
+        # SSH Tunnel
+        """ tunnel = sshtunnel.SSHTunnelForwarder(
+            ("sshserver"), 
+            ssh_username="username", 
+            ssh_password="password", 
+            remote_bind_address=("ipadress", "port")
+        )
+
+        tunnel.start() """
+
         return psycopg2.connect(
             host=config('PGSQL_HOST'),
             user=config('PGSQL_USER'),
